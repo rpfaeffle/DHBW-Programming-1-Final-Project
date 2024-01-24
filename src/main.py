b601566b9bd39c4605a6f68a91e3eaf6a4b6ed8d
Tim Kerl <tim.kerl.02@gmx.de>
2024-01-24 17:39:35 +0100

    feat: add logic for more fallen objects

--- src/main.py
+++ src/main.py
@@ -1,37 +1,91 @@
+from OpenGL.GLUT import *
 from OpenGL.GL import *
 from core.application import Application
 from core.component import Component, Render
 from core.openGLUtils import OpenGLUtils
+import random
 
 # Constants
-GAME_SPEED = 60 # Block moves down every 60 frames
+GAME_SPPED = 5 # Block moves down every 60 frames
+
+COLORS = {
+  'I': (0.0, 1.0, 1.0, 1.0),
+  'J': (0.0, 0.0, 1.0, 1.0),
+  'L': (1.0, 0.64, 0.0, 1.0),
+  'O': (1.0, 1.0, 0.0, 1.0),
+  'S': (0.0, 1.0, 0.0, 1.0),
+  'T': (0.5, 0.0, 0.5, 1.0),
+  'Z': (1.0, 0.0, 0.0, 1.0),
+}
+
+SHAPES = {
+  'I': [[True, True, True, True]],
+  'J': [[True, False, False], [True, True, True]],
+  'L': [[False, False, True], [True, True, True]],
+  'O': [[True, True], [True, True]],
+  'S': [[False, True, True], [True, True, False]],
+  'T': [[False, True, False], [True, True, True]],
+  'Z': [[True, True, False], [False, True, True]],
+}
+
+KEYS = list(SHAPES.keys())
+
+def flatten(list):
+    return [item for sublist in list for item in sublist if item is not None]
 
 class Tetris(Render):
     def __init__(self, cx):
         self.cx = None
         self.blocks = []
+        self.falling_block = None
+
+        self.rows = cx.width // 20
+        self.columns = cx.height // 20
+
         self.initialize(cx)
 
     def initialize(self, cx):
+        self.blocks = [[None for _ in range(self.rows)] for _ in range(self.columns)]
         self.spawn_new_block(cx)
 
     def render(self, cx):
-        print(cx.frame)
-        if cx.frame % GAME_SPEED == 0:
+        if cx.frame % GAME_SPPED == 0:
           self.update()
 
-        return self.blocks
+        return flatten([flatten(self.blocks), self.falling_block.blocks if self.falling_block else []])
 
     def spawn_new_block(self, cx):
-        spawn_position = (0, cx.height)
-        self.blocks.append(Block((1.0, 0.0, 0.0, 1.0), spawn_position[0], spawn_position[1]))
-        pass
+        block_type = random.choice(KEYS)
+        spawn_position = (self.rows // 2, self.columns - 1)
+        print(spawn_position)
+        new_block = []
+
+        for row in range(len(SHAPES[block_type])):
+          for column in range(len(SHAPES[block_type][row])):
+            if SHAPES[block_type][row][column]:
+              new_block.append(Block(COLORS[block_type], spawn_position[0] + column, spawn_position[1] - row))
+
+        self.falling_block = FallingBlock(new_block)
 
     def update(self):
-        print(self.blocks)
-        for block in self.blocks:
-          if block.isFalling:
-            block.move_down()
+      if self.check_collision():
+        self.register_blocks(self.falling_block.blocks)
+        self.falling_block = None
+        self.spawn_new_block(self.cx)
+      else:
+        self.falling_block.fall()
+
+    def register_blocks(self, blocks):
+      for block in blocks:
+        self.blocks[block.y][block.x] = block
+
+    def check_collision(self):
+      for block in self.falling_block.get_blocks():
+        if block.y <= 0:
+          return True
+
+        if self.blocks[block.y - 1][block.x] is not None:
+          return True
 
     @staticmethod
     def new(cx):
@@ -43,10 +97,9 @@ class Block(Component):
     self.color = color
     self.x = x
     self.y = y
-    self.isFalling = True
 
   def render(self, cx):
-    x, y = OpenGLUtils.convert_to_normalized_coordinates(self.x, self.y, cx.width, cx.height)
+    x, y = OpenGLUtils.convert_to_normalized_coordinates(self.x * 20, self.y * 20, cx.width, cx.height)
     width, height = OpenGLUtils.convert_to_normalized_size(20, 20, cx.width, cx.height)
 
     glPushMatrix()
@@ -61,8 +114,21 @@ class Block(Component):
     glPopMatrix()
 
   def move_down(self):
-    print(self.y)
-    self.y -= 20
+    self.y -= 1
+
+class FallingBlock(object):
+  def __init__(self, blocks):
+    self.blocks = blocks
+
+  def fall(self):
+    for block in self.blocks:
+      block.move_down()
+
+  def can_fall(self):
+    return True
+
+  def get_blocks(self):
+    return self.blocks
 
 if __name__ == '__main__':
   Application().run(lambda cx: Tetris.new(cx))

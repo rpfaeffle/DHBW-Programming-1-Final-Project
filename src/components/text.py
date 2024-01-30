ae56e42b85c4994fd6699196930c014c72152b6d
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-01-30 16:25:07 +0100

    refactor: remove unused properties

--- src/components/text.py
+++ src/components/text.py
@@ -8,15 +8,9 @@ class Text(Component):
         super().__init__()
         self.text = text
         self.x, self.y = x, y
-        self.estimated_width = 0
 
     def render(self, cx):
         lines = self.text.split('\n')
-
-        if self.estimated_width == 0:
-          self.estimated_width = functools.reduce(lambda acc, line: max(acc, len(line)), lines, 0)
-          self.estimated_width *= cx.font.dimension.width
-
         x, y = OpenGLUtils.convert_to_normalized_coordinates(self.x, self.y, cx.width, cx.height)
 
         for i in range(len(lines)):

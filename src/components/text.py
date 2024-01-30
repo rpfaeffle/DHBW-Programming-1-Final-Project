980935758f1f6b7321ad105d912e659ec1b84489
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-01-30 15:12:37 +0100

    feat: add support for multiline text
    By default text would be rendered in one line even if it contains a line
    break. Now it is rendered as multiple lines.

--- src/components/text.py
+++ src/components/text.py
@@ -1,3 +1,4 @@
+import functools
 from core.component import Component
 from core.openGLUtils import OpenGLUtils
 
@@ -7,8 +8,19 @@ class Text(Component):
         super().__init__()
         self.text = text
         self.x, self.y = x, y
+        self.estimated_width = 0
 
     def render(self, cx):
+        lines = self.text.split('\n')
+
+        if self.estimated_width == 0:
+          self.estimated_width = functools.reduce(lambda acc, line: max(acc, len(line)), lines, 0)
+          self.estimated_width *= cx.font.dimension.width
+
         x, y = OpenGLUtils.convert_to_normalized_coordinates(self.x, self.y, cx.width, cx.height)
-        cx.font.draw(self.text, x, y)
+
+        for i in range(len(lines)):
+          line = lines[i]
+          cx.font.draw(line, x, y - i * cx.font.dimension.height)
+
         return self
